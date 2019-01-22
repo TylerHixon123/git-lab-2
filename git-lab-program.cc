6d945a660a2f3255a9157fde73c19f41ed647547
Tyler Hixon <th702717@ohio.edu>
2019-01-22 11:24:35 -0500

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Tyler Hixon
+ *        Date: January 22, 2019
+ * Description: lab stuff
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+	cout << "Hello Git!!" << endl;
+	return 0;
+}
